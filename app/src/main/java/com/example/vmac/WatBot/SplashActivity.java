23da6b71598c82fb6a3f3f01c20ae42cd43babbf
Dattaraj Gauns <103878434+GaunsDattaraj@users.noreply.github.com>
2022-04-17 15:48:41 +0530

    vba-android

---
+++ app/src/main/java/com/example/vmac/WatBot/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.example.vmac.WatBot;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    private final int SPLASH_DISPLAY_LENGTH = 5000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashActivity.this,DeviceList.class);
+               startActivity(mainIntent);
+               finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+
+
+    }
+}
